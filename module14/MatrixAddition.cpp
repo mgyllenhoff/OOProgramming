cb3fbea19e0000a95fb6d0680ebca120c516ffa4
Madeline Gyllenhoff <mgyllenhoff@gmail.com>
2025-11-30 19:56:18 -0500

    Update

--- module14/MatrixAddition.cpp
+++ module14/MatrixAddition.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <ctime>
+#include <thread>
+#include <vector>
 using namespace std;
 
 const int NUM_ROWS = 10;
@@ -61,11 +63,11 @@ int main(void) {
     cout << "MATRIX B:" << endl;
     printMatrix(B);
 
-    double sum1 = 0;
+    double unthreadedTotalSum = 0;
 
     // Add matrices normally
     clock_t startTime1 = clock();
-    matrixAdd(A, B, C1, 0, NUM_ROWS - 1, sum1);
+    matrixAdd(A, B, C1, 0, NUM_ROWS - 1, unthreadedTotalSum);
     clock_t endTime1 = clock();
 
     cout << "OUTPUT MATRIX:" << endl;
@@ -73,6 +75,42 @@ int main(void) {
     cout << "Adding matrices normally took " << elapsedSec(startTime1, endTime1) << "seconds" << endl;
 
     // Add matrices using threading
-    
+    int numThreads = 4; // Manually choose # threads (safe for small matrices)
+    vector<thread> threads; // Holds all the thread objects
+    vector<double> threadSums(numThreads, 0); // To store partial sum of matrix additions for each thread
+
+    int rowsPerThread = NUM_ROWS / numThreads; // Divides work among threads
+
+    clock_t startTime2 = clock();
+
+    // Perform threading
+    for (int t = 0; t < numThreads; t++) {
+        // Determine the rows each thread will execute on
+        int startRow = t * rowsPerThread;
+        int endRow = (t == numThreads - 1) ? NUM_ROWS - 1 : startRow + rowsPerThread - 1;
+
+        threads.emplace_back(matrixAdd, A, B, C2, startRow, endRow, ref(threadSums[t]));
+    }
+
+    // Wait for all threads to finish
+    for (auto &th : threads) {
+        th.join();
+    }
+
+    clock_t endTime2 = clock();
+
+    // Combine sums
+    double threadedTotalSum = 0;
+    for (double s : threadSums) {
+        threadedTotalSum += s;
+    }
+
+    cout << "THREADED OUTPUT MATRIX:" << endl;
+    printMatrix(C2);
+
+    cout << "Adding matrices using threading took " << elapsedSec(startTime2, endTime2) << " seconds" << endl;
+
+    cout << "Unthreaded sum = " << unthreadedTotalSum << "\n";
+    cout << "Threaded sum = " << threadedTotalSum << "\n";
 
 } // end main
